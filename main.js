ac122061d2c29ffdccecaeea948c4d1ef07eb3e6
emanuele5180 <macina5180>
2020-08-26 15:52:43 +0200

    chiamata ajax get

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+function getTasks() {
+  $.ajax({
+    url: 'http://157.230.17.132:3024/todos',
+    method: 'GET',
 
+    success: function(data) {
+      console.log('data', data);
+    },
+    error: function(err) {
+      console.log('err', err);
+    }
+
+  })
+}
 
 
 function init() {
-  
+
   console.log("hello world");
+  getTasks();
 
 
 
