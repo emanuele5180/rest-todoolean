cfde3be056d774560d7e43158de3c1af05ff7590
emanuele5180 <macina5180>
2020-08-26 17:22:49 +0200

    agiunta task

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+//
+function addInsertListener(){
+  var target = $('#add');
+  target.click(insertTask);
+}
+
+
+//aggiunta elementi alla lista
+
+function insertTask() {
+  var target = $ ('#task-text');
+  var text = target.val();
+
+  $.ajax({
+    url:'http://157.230.17.132:3024/todos',
+    method: 'POST',
+    data: {
+      text: text
+    },
+
+    success: function(data) {
+      console.log('data', data);
+
+    },
+    error: function(err) {
+      console.log('err', err);
+    }
+
+  })
+
+}
+
+// chiamata ajax per avere i dati
 function getTasks() {
   $.ajax({
     url: 'http://157.230.17.132:3024/todos',
@@ -14,20 +47,26 @@ function getTasks() {
   })
 }
 
+
+// stampa dei dati nella lista su foglio html
 function printTasks(tasks){
   var target = $('#tasks');
   for (var i = 0; i < tasks.length; i++) {
     var task = tasks[i];
-    target.append('<li>${task.text} </li>');
+    target.append(`<li>${task.text} </li>`);
   }
 
 }
+
+
+
 
 
 function init() {
 
   console.log("hello world");
   getTasks();
+  addInsertListener();
 
 
 
