02a8397fb2524820e2578c01c83bb2889638e353
emanuele5180 <macina5180>
2020-08-26 18:08:41 +0200

    delete

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function insertTask() {
   var target = $ ('#task-text');
   var text = target.val();
 
+
   $.ajax({
     url:'http://157.230.17.132:3024/todos',
     method: 'POST',
@@ -20,6 +21,7 @@ function insertTask() {
 
     success: function(data) {
       console.log('data', data);
+      getTasks();
 
     },
     error: function(err) {
@@ -38,7 +40,7 @@ function getTasks() {
 
     success: function(data) {
       console.log('data', data);
-      printTasks(data)
+      printTasks(data);
     },
     error: function(err) {
       console.log('err', err);
@@ -51,15 +53,38 @@ function getTasks() {
 // stampa dei dati nella lista su foglio html
 function printTasks(tasks){
   var target = $('#tasks');
+  target.text('');
   for (var i = 0; i < tasks.length; i++) {
     var task = tasks[i];
-    target.append(`<li>${task.text} </li>`);
+    target.append(`<li>${task.text} - <span data-id="${task.id}" class="click"><b> x </b></span> </li>`);
   }
 
 }
 
+function addDeleteListener() {
+  $(document).on('click', '.click', deleteTask)
+}
 
+function deleteTask() {
+  var button = $(this);
+  var id =button.data('id');
 
+  console.log('id', id);
+
+  $.ajax({
+    url: `http://157.230.17.132:3024/todos/${id}`,
+    method: 'DELETE',
+
+    success: function(data) {
+      console.log('data', data);
+      getTasks();
+
+    },
+    error: function(err) {
+      console.log('err', err);
+    }
+  })
+}
 
 
 function init() {
@@ -67,6 +92,7 @@ function init() {
   console.log("hello world");
   getTasks();
   addInsertListener();
+  addDeleteListener();
 
 
 
